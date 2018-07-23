b6bae11c54d52d6eb4f936d05ceb1f036ad8fd2d
George Francis <george.francis@gamer-network.net>
2018-07-23 16:43:55 +0100

    added build-inline script

--- babel_scss_webpack_setup/webpack.common.js
+++ babel_scss_webpack_setup/webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
--- babel_scss_webpack_setup/webpack.prod-inline.js
+++ babel_scss_webpack_setup/webpack.prod-inline.js
@@ -1,11 +1,16 @@
 const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'styles.bundle.css',
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      inline: 'main.bundle',
+      defaultAttribute: 'sync',
     }),
   ],
   optimization: {

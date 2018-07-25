c08de5fcea38f589cc0c068cdd603f6fa904a77e
George Francis <george.francis@gamer-network.net>
2018-07-25 14:37:53 +0100

    added internal flag for ad builds replacing a whole config

--- babel_scss_webpack_setup/webpack.prod.js
+++ babel_scss_webpack_setup/webpack.prod.js
@@ -2,6 +2,10 @@ const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
+const INTERNAL_FLAG = process.argv[process.argv.indexOf('--internal') + 1];
+
+console.log(INTERNAL_FLAG);
+
 module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({

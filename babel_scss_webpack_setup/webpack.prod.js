cbbca0d5ca26b3e2e6040c79b2fd021728d938f7
George Francis <george.francis@gamer-network.net>
2018-07-26 09:08:52 +0100

    removed inline build scripts

--- babel_scss_webpack_setup/webpack.prod.js
+++ babel_scss_webpack_setup/webpack.prod.js
@@ -2,10 +2,6 @@ const merge = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const common = require('./webpack.common.js');
 
-const INTERNAL_FLAG = process.argv[process.argv.indexOf('--internal') + 1];
-
-console.log(INTERNAL_FLAG);
-
 module.exports = merge(common, {
   plugins: [
     new MiniCssExtractPlugin({

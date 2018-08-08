d3b03f1e4d69c40e189e420eef7dbc9d585bf907
George Francis <george.francis@gamer-network.net>
2018-08-08 13:33:44 +0100

    added generateIframe code to html5 generator

--- code/tools/build_tools/creative_zip_uploader/components/generator_html5/generator.js
+++ code/tools/build_tools/creative_zip_uploader/components/generator_html5/generator.js
@@ -2,44 +2,77 @@ const fs = require('fs-extra');
 const path = require('path');
 
 module.exports = app => {
+  let iFrames = [];
+
   app.post('/generateHTML5', (req, res) => {
     const PAGE_TITLE = req.body.campaign;
     const CREATIVE_URLS = req.body.creative;
     const PREVIEW_PAGE_DIR = path.join(__dirname, `/../../static/pages/${PAGE_TITLE}/`);
 
-    if(PAGE_TITLE.length > 1) {
-      makePageDir(PREVIEW_PAGE_DIR, CREATIVE_URLS, PAGE_TITLE);
+    if (PAGE_TITLE.length > 1) {
+      if(CREATIVE_URLS !== '') {
+        makePageDir(PREVIEW_PAGE_DIR, CREATIVE_URLS, PAGE_TITLE);
+      } else {
+        return res.status(500).send('Please Add Some Creatives!');
+      }
     } else {
-      console.log('No Title Specified');
+      return res.status(500).send('Please Add A Page Title!');
     }
   });
 
   function makePageDir(dir, creatives, title) {
-    if(fs.existsSync(dir)) {
-      console.log('Preview Page Title Already Exists');
+    if (fs.existsSync(dir)) {
+      // Just copy the creative dirs, dont creative a new directory for the page title
+      copyCreativeDirs(dir, creatives, title);
     } else {
       fs.mkdirSync(dir, err => {
-        if(err) {
-          console.log(err);
+        if (err) {
+          return res.status(500).send('Error Creating Page Directory: ', err);
         }
       });
       copyCreativeDirs(dir, creatives, title);
     }
+    generateIframes(creatives);
   }
 
   function copyCreativeDirs(dir, creatives, title) {
-    fs.mkdirSync(dir + '/creatives', err => {
-      if(err) {
-        console.log(err);
-      }
-    })
-    creatives.forEach(url => {
-      console.log(url.split('unzipped/')[1])
+    if (!fs.existsSync(dir + 'creatives')) {
+      fs.mkdirSync(dir + '/creatives', err => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+    if (creatives.constructor === Array) {
+      creatives.forEach(url => {
+        try {
+          fs.copySync(__dirname + '/../../static/' + url + '/', './static/pages/' + title + '/creatives/' + url.split('unzipped/')[1]);
+        } catch (err) {
+          throw err;
+        }
+      });
+    } else {
       try {
-        fs.copySync(__dirname +  '/../../static/' + url + '/', './static/pages/' + title + '/creatives/' + url.split('unzipped/')[1]);
-      } catch(err) {
-        console.log(err)
+        fs.copySync(__dirname + '/../../static/' + creatives + '/', './static/pages/' + title + '/creatives/' + creatives.split('unzipped/')[1]);
+      } catch (err) {
+        throw err;
       }
-    });
+    }
   }
-} 
+
+  function generateIframes(creatives) {
+    if(creatives.constructor === Array) {
+      creatives.forEach(creative => {
+        iFrames.push(createIframe(creative));
+      })
+    } else {
+      iFrames.push(createIframe(creatives));
+    }
+    
+  }
+
+  function createIframe() {
+    let entryPath = 'creatives/' + creative.split('unzipped/')[1] + 'index.html';
+    let newIframe = `<iframe src="${entryPath}" scrolling="no">`;
+  }
+}

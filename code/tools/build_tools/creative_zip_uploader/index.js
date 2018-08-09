0f4292452e5bce6ae0c4123538c5d5c016014b60
George Francis <george.francis@gamer-network.net>
2018-08-09 09:13:33 +0100

    moved zip uploader development to gamernetwork/ads-preview-tool-lite

--- code/tools/build_tools/creative_zip_uploader/index.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app/app');
-
-const routes = require('./routes/routes')(app);
-
-const generator = require('./components/generator_html5/generator')(app);
-const uploader = require('./components/uploader/uploader')(app);
-
-

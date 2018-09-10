55367d1cfad1281168c4c85e808d55756dcf50d2
George Francis <george.francis@gamer-network.net>
2018-09-10 10:47:19 +0100

    init commit for potential build script for takeover code

---
+++ code/tools/build_tools/takeover_boilerplate_code_build/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "eslint:recommended",
+  "parser": "babel-eslint",
+  "env": {
+    "browser": "true"
+  },
+  "rules": {
+      // enable additional rules
+      "indent": ["error", 2],
+      "no-console": ["warn"]
+  }
+}
---
+++ code/tools/build_tools/takeover_boilerplate_code_build/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const CURRENT_VERSION = '1.0';
+const VERSION_PATH = `dist/${CURRENT_VERSION}`;
+
+module.exports = env => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, VERSION_PATH),
+      filename: 'takeover-boilerplate.min.js',
+      library: 'takeoverCode'
+    },
+    devtool: env.NODE_ENV === 'development' ? 'inline-source-map' : 'source-map',
+    devServer: {
+      contentBase: './dist'
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader'
+        },
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          },
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin([VERSION_PATH]),
+      new UnminifiedWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/test.html'
+      })
+    ]
+  }
+}

d2384c84f79be0b683f4ebae1e4fc40eff5ed1dd
George Francis <george.francis@gamer-network.net>
2018-09-13 11:10:48 +0100

    added init defaults for a cleaner init function

--- code/tools/build_tools/takeover_boilerplate_code_build/src/index.js
+++ code/tools/build_tools/takeover_boilerplate_code_build/src/index.js
@@ -15,16 +15,16 @@ export const init = (props, clickUrlEsc) => {
   // Apply skin styles if not in local dev mode
   !props.dev && (
     applyBaseStyles({
-      site: props.site, 
-      pageWrapper: props.pageWrapper, 
-      takeoverBGColor: props.takeoverBGColor, 
-      skinImg: props.skinImage,
-      skinLink: props.skinLink,
-      skinHeight: props.skinHeight,
-      skinTopOffset: props.skinTopOffset,
-      leaderboardHeight: props.leaderboardHeight,
-      supersize: props.supersize,
-      skinlong: props.skinLong,
+      site: props.site !== undefined ? props.site : '%%SITE%%', 
+      pageWrapper: props.pageWrapper !== undefined ? props.pageWrapper : '#page-wrapper', 
+      takeoverBGColor: props.takeoverBGColor !== undefined ? props.takeoverBGColor : 'black', 
+      skinImg: props.skinImg !== undefined ? props.skinImage : '%%FILE:SKIN%%',
+      skinLink: props.skinLink !== undefined ? props.skinLink : '%%DEST_URL%%',
+      skinHeight: props.skinHeight !== undefined ? props.skinHeight : 1300,
+      skinTopOffset: props.skinTopOffset !== undefined ? props.skinTopOffset: '0',
+      leaderboardHeight: props.leaderboardHeight !== undefined ? props.leaderboardHeight : '250',
+      supersize: props.supersize !== undefined ? props.supersize : false,
+      skinlong: props.skinLong !== undefined ? props.skinLong : true,
     }, clickUrlEsc)
   );
   syncIframes(props.site, props.skinImage);

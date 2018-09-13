6496327cdb88c1ecd57ca90d148e409394c50c3c
George Francis <george.francis@gamer-network.net>
2018-09-13 11:15:49 +0100

    swapping undefined for null in props check

--- code/tools/build_tools/takeover_boilerplate_code_build/src/index.js
+++ code/tools/build_tools/takeover_boilerplate_code_build/src/index.js
@@ -16,16 +16,16 @@ export const init = (props, clickUrlEsc) => {
   // Apply skin styles if not in local dev mode
   !props.dev && (
     applyBaseStyles({
-      site: props.site !== undefined ? props.site : '%%SITE%%', 
-      pageWrapper: props.pageWrapper !== undefined ? props.pageWrapper : '#page-wrapper', 
-      takeoverBGColor: props.takeoverBGColor !== undefined ? props.takeoverBGColor : 'black', 
-      skinImg: props.skinImg !== undefined ? props.skinImage : '%%FILE:SKIN%%',
-      skinLink: props.skinLink !== undefined ? props.skinLink : '%%DEST_URL%%',
-      skinHeight: props.skinHeight !== undefined ? props.skinHeight : 1300,
-      skinTopOffset: props.skinTopOffset !== undefined ? props.skinTopOffset: '0',
-      leaderboardHeight: props.leaderboardHeight !== undefined ? props.leaderboardHeight : '250',
-      supersize: props.supersize !== undefined ? props.supersize : false,
-      skinlong: props.skinLong !== undefined ? props.skinLong : true,
+      site: props.site != null ? props.site : '%%SITE%%', 
+      pageWrapper: props.pageWrapper != null ? props.pageWrapper : '#page-wrapper', 
+      takeoverBGColor: props.takeoverBGColor != null ? props.takeoverBGColor : 'black', 
+      skinImg: props.skinImg != null ? props.skinImage : '%%FILE:SKIN%%',
+      skinLink: props.skinLink != null ? props.skinLink : '%%DEST_URL%%',
+      skinHeight: props.skinHeight != null ? props.skinHeight : 1300,
+      skinTopOffset: props.skinTopOffset != null ? props.skinTopOffset: '0',
+      leaderboardHeight: props.leaderboardHeight != null ? props.leaderboardHeight : '250',
+      supersize: props.supersize != null ? props.supersize : false,
+      skinlong: props.skinLong != null ? props.skinLong : true,
     }, clickUrlEsc)
   );
   syncIframes(props.site, props.skinImage);
